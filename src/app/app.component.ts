1ba8c306b1c021846a79c5c2baabbc116edf3d32
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2025-05-18 11:53:41 +0530

    chor: husky, eslint, tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,15 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold mb-4">{{ title }}</h1>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PMS-Frontend';
